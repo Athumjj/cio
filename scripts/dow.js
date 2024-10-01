c3c3e4b12d9ee398dab7013cca0dc2605cb10283
Athumjj <melloteam22@gmail.com>
2024-10-01 17:32:34 -0300

    cio

--- scripts/dow.js
+++ scripts/dow.js
@@ -1,22 +1,30 @@
 class dow {
     constructor(item, cv) {
         const img = new Image();
-        img.onload = this.convert;
+        img.onload = this.convert.bind(img, cv); // Passa o cv também
         img.src = URL.createObjectURL(item);
     }
-    
-    convert() {
-        URL.revokeObjectURL(this.src);
+
+    convert(cv) {
         const a = document.createElement("canvas"),
         ctx = a.getContext("2d");
         a.width = this.width;
         a.height = this.height;
         ctx.drawImage(this, 0, 0);
-        
+        URL.revokeObjectURL(this.src); // Revoga depois do uso
+
+        const quality = cv === 'jpeg' ? 0.75 : undefined; // Define a qualidade apenas para jpeg
+
         a.toBlob((blob) => {
             const file = new File([blob], `image.${cv}`, { type: "application/octet-stream" });
-            window.location = URL.createObjectURL(file);
-        }, `image/${cv}`, 0.75);
+            
+            const link = document.createElement('a'); // Cria um link para download
+            link.href = URL.createObjectURL(file);
+            link.download = `image.${cv}`;
+            link.click();
+            URL.revokeObjectURL(link.href); // Revoga a URL gerada para o download
+            link.remove();
+        }, `image/${cv}`, quality);
     }
 }
 
