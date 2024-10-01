acb2e716ca4e4fb5982ffc26b2c11099031c808b
Athumjj <melloteam22@gmail.com>
2024-10-01 17:25:58 -0300

    cio

--- scripts/dow.js
+++ scripts/dow.js
@@ -1,11 +1,22 @@
 class dow {
     constructor(item, cv) {
-        const imgUrl = URL.createObjectURL(item);
-        const a = document.createElement("a");
-        a.href = imgUrl;
-        a.download = "img." + cv;
-        a.click();
-        a.remove();
+        const img = new Image();
+        img.onload = this.convert;
+        img.src = URL.createObjectURL(item);
+    }
+    
+    convert() {
+        URL.revokeObjectURL(this.src);
+        const a = document.createElement("canvas"),
+        ctx = a.getContext("2d");
+        a.width = this.width;
+        a.height = this.height;
+        ctx.drawImage(this, 0, 0);
+        
+        a.toBlob((blob) => {
+            const file = new File([blob], `image.${cv}`, { type: "application/octet-stream" });
+            window.location = URL.createObjectURL(file);
+        }, `image/${cv}`, 0.75);
     }
 }
 
