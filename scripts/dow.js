66d50cfce7334ab5f0093cae5762f46607002eea
Athumjj <melloteam22@gmail.com>
2024-10-01 16:51:38 -0300

    cio

--- scripts/dow.js
+++ scripts/dow.js
@@ -1,7 +1,8 @@
 class dow {
     constructor(item, cv) {
+        const imgUrl = URL.createObjectURL(item);
         const a = document.createElement("a");
-        a.href = item;
+        a.href = imgUrl;
         a.download = "img." + cv;
         a.click();
         a.remove();
